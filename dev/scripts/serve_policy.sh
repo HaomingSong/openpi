a438dd57943f52b78ff25368a26a171df4f941d4
haoming <1847575517@qq.com>
2025-02-22 22:30:27 +0800

    change scripts

--- dev/scripts/serve_policy.sh
+++ dev/scripts/serve_policy.sh
@@ -1,1 +1,3 @@
-uv run scripts/serve_policy.py policy:checkpoint --policy.config=pi0_fast_bridge_low_mem_finetune_hand --policy.dir=checkpoints/pi0_fast_bridge_low_mem_finetune_hand/pi0_fast_bridge_low_mem_finetune_hand/4000
+uv run scripts/serve_policy.py policy:checkpoint \
+    --policy.config=pi0_fast_bridge_low_mem_finetune_hand \
+    --policy.dir=checkpoints/pi0_fast_bridge_low_mem_finetune_hand/pi0_fast_bridge_low_mem_finetune_hand/4000
